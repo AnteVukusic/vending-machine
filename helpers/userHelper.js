c42ac643c837b0c2d1b1b4a9efae79720692e241
Ante Vukušić <vukusic.ante@gmail.com>
2022-05-15 21:58:26 +0200

    [vending-machine] - update bcrypt

--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Ajv = require('ajv');
 const jwt = require('jsonwebtoken');
 const { userModels } = require('../models/userModels');
